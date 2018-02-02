26efdccba69f2a72f1473b5dffcca9d740c4e897
Daniel <danrica92@yahoo.com>
2018-02-01 19:10:58 -0800

    Added schedule query to client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
-import { graphql } from 'react-apollo';
+import { graphql, compose } from 'react-apollo';
 import logo from './logo.svg';
 import './App.css';
 
 import { formatTeamId } from './utils/index';
 
 class App extends Component {
+  state = {};
+
   renderPlays() {
-    return this.props.data.leadTracker.plays.map(play => {
+    return this.props.leadTrackerQuery.leadTracker.plays.map(play => {
       return (
         <li>
           Time: {play.clock}, Points: {play.points}, Team:{formatTeamId(play.leadTeamId)}
@@ -18,12 +20,13 @@ class App extends Component {
   }
 
   render() {
-    if (this.props.data.loading) {
+    if (this.props.leadTrackerQuery.loading) {
       return <div />;
     }
 
     return (
       <div className="App">
+        {console.log(this.props)}
         <div>
           <ul>
             {this.renderPlays()}
@@ -34,7 +37,7 @@ class App extends Component {
   }
 }
 
-const query = gql`
+const getLeadTracker = gql`
   {
     leadTracker(date: "20180201", gameId: "0021700762", period: "2") {
       plays {
@@ -46,4 +49,26 @@ const query = gql`
   }
 `;
 
-export default graphql(query)(App);
+const getSchedule = gql`
+  {
+    schedule(year: "2017") {
+      league {
+        standard {
+          gameId
+          startDateEastern
+          hTeam {
+            teamId
+          }
+          vTeam {
+            teamId
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default compose(
+  graphql(getLeadTracker, { name: 'leadTrackerQuery' }),
+  graphql(getSchedule, { name: 'scheduleQuery' })
+)(App);
